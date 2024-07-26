a5b8be2af168e1c56aa8dea637abfcf5520770b9
Albin Tendron <albin.tendron@gmail.com>
2024-07-26 16:22:02 +0200

    feat: staging workflow

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
+    bucket = "backend-terraform-albin"
+    key    = "vprofile-gitops.tfstate"
+    region = "eu-west-1"
   }
 
   required_version = "~> 1.6.3"
